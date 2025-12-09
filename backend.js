d58f0298ccabd681b9d7f52ba9b2d8188f6bbc6c
Bruno Henrique Oliveira Dos Santos <BrunoHOSantos@hotmail.com>
2025-12-09 20:49:15 -0300

    Sexto Passo

--- backend.js
+++ backend.js
@@ -5,3 +5,20 @@ document.getElementById("toggleTheme").addEventListener('click', function() {
     document.documentElement.classList.toggle('dark')
 });
 
+
+document.getElementById("sendComment").addEventListener("click", function() {
+    const text = document.getElementById("commentInput").value.trim();
+
+    if (text === "") return;
+
+    const feed = document.getElementById("commentFeed");
+
+    const item = document.createElement("div");
+    item.classList.add("list-group-item");
+    item.textContent = text;
+
+    feed.prepend(item); // adiciona no topo
+
+    document.getElementById("commentInput").value = "";
+});
+
